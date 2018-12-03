a311b2c83951f87a877198589951eb17d452c04b
markmaxt <185182176@qq.com>
2018-12-03 13:53:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-import algo
-# from arguments import get_args
+from arguments import get_args
+
+# import algo
+from arguments import get_args
+from visualize import visdom_plot
+
+args = get_args()
+
+torch.manual_seed(args.seed)
+
+
+def main():
+  torch.set_num_threads(1)
+  device = torch.device("cuda:0" if args.cuda else "cpu")
+  
+  if args.vis:
+    from visdom import Visdom
+    viz = Visdom(port=args.port)
